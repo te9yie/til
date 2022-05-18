b64c57a049eb50d2a661a7edbf492aa2746ddd9a
te9yie <te9yie@users.noreply.github.com>
2022-05-18 20:25:13 +0900

    new

---
+++ csharp/table-data-def/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClosedXML.Excel;
+using System;
+
+namespace table_data_def
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var book = new XLWorkbook("sample.xlsx"))
+            {
+                foreach (var sheet in book.Worksheets)
+                {
+                    Console.Out.WriteLine(sheet.Name);
+                    var range = sheet.RangeUsed();
+                    foreach (var row in range.RowsUsed())
+                    {
+                        foreach (var cell in row.CellsUsed())
+                        {
+                            var address = cell.Address;
+                            Console.Out.Write($"{address.RowNumber}-{address.ColumnNumber}={cell.Value}, ");
+                        }
+                        Console.Out.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}

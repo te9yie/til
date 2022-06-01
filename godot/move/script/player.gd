d0b1d56cb328491aa99c3456436b9189b2507a1b
te9yie <te9yie@users.noreply.github.com>
2022-06-01 09:14:19 +0900

    new

---
+++ godot/move/script/player.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+const SPEED = 200.0
+
+func _ready():
+	pass
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	position += velocity.normalized() * SPEED * delta;

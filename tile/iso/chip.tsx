45559b55735c593c31940ee66c6fdaa1483b80ba
te9yie <te9yie@users.noreply.github.com>
2022-06-20 23:55:22 +0900

    update

--- tile/iso/chip.tsx
+++ tile/iso/chip.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="chip" tilewidth="32" tileheight="32" tilecount="10" columns="0">
+<tileset version="1.8" tiledversion="1.8.6" name="chip" tilewidth="32" tileheight="48" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="grass.png"/>
@@ -30,5 +30,11 @@
  </tile>
  <tile id="9">
   <image width="32" height="32" source="wall_side.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="48" source="chara.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="chara2.png"/>
  </tile>
 </tileset>
---
+++ tile/iso/chip2.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="chip2" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+ <tileoffset x="0" y="-16"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="box.png"/>
+ </tile>
+</tileset>

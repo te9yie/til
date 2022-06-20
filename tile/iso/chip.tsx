cbd139e7471417e5eef4645a884ba609bfa7a027
te9yie <te9yie@users.noreply.github.com>
2022-06-20 23:11:58 +0900

    new

---
+++ tile/iso/chip.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="chip" tilewidth="32" tileheight="32" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="wall2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="box.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="wall_bottom.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="wall_top.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="wall2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="floor.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="wall_outside2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="wall_top2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="wall_side.png"/>
+ </tile>
+</tileset>
